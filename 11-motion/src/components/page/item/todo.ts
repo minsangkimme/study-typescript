d3d9d83c02d05412a0abb7243d16cab1aea6faf7
minsangkimme <minsang.tech@gmail.com>
2022-07-24 15:20:49 +0900

    fix: style 수정

--- 11-motion/src/components/page/item/todo.ts
+++ 11-motion/src/components/page/item/todo.ts
@@ -3,14 +3,15 @@ import { BaseComponent } from '../../component.js';
 export class ToDoComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, todo: string) {
     super(`<section class="todo">
-                <h2 class="todo__title"></h2>
-                <input type="checkbox" class="todo-checkbox">
+                <h2 class="page-item__title todo__title"></h2>  
+            <input type="checkbox" id="todo-checkbox"/>
+            <label for="todo-checkbox" class="todo-label"></label>
         </section>`);
 
     const titileElement = this.element.querySelector('.todo__title')! as HTMLHeadingElement;
     titileElement.textContent = title;
 
-    const todoElement = this.element.querySelector('.todo-checkbox')! as HTMLParagraphElement;
-    todoElement.insertAdjacentText('afterend', todo);
+    const todoElement = this.element.querySelector('.todo-label')! as HTMLLabelElement;
+    todoElement.textContent = todo;
   }
 }
